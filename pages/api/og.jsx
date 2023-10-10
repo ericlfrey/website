c061240a0e1e98d3adef460001542c6c9f81c790
Eric Frey <ericlfrey@gmail.com>
2023-10-10 16:19:14 -0500

    Created a component/page to be a thumbnail. WIP need to test.

---
+++ pages/api/og.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable @next/next/no-img-element */
+import { ImageResponse } from '@vercel/og';
+import Image from 'next/image';
+
+export const config = {
+  runtime: 'edge',
+};
+
+export default async function handler() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          fontSize: 60,
+          color: '#DADADB',
+          background: '#262627',
+          width: '100%',
+          height: '100%',
+          paddingTop: 50,
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <img
+          src={'https://avatars.githubusercontent.com/u/107942776?v=4'}
+          className="profile-img"
+          style={{
+            borderRadius: '50%',
+            border: '1px solid #2fac66',
+            marginBottom: 50,
+          }}
+          width={300}
+          height={300}
+          alt="eric personal headshot"
+          priority
+        />
+        <p style={{}}>Eric's Portfolio Site!</p>
+      </div>
+    ),
+    {
+      width: 700,
+      height: 630,
+    }
+  );
+}
