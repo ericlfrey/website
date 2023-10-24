5c8e3977708e0af27edb7c188d85667f30690bcf
Eric Frey <ericlfrey@gmail.com>
2023-10-24 09:32:00 -0500

    Edited the meta tag for the preview image.

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name='image' property='og:image' content='http://www.ericlfrey.com/_next/image?url=%2Fimages%2Fasset.jpg&w=640&q=75' />
+        <meta name='image' property='og:image' content='https://www.ericlfrey.com/_next/image?url=%2Fimages%2Fasset.jpg&w=640&q=75' />
         <meta name="title" property="og:title" content="Eric's Portfolio"></meta>
       </Head>
       <Layout>
