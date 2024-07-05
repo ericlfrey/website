d6fc5c14269b19a8b4b900b4a388fb5f5df9f763
Eric Frey <ericlfrey@gmail.com>
2024-07-05 10:17:52 -0500

    Removed deployed links for tthe heroku deployed projects

--- pages/api/data.js
+++ pages/api/data.js
@@ -6,7 +6,7 @@ export const projectData = [
             'Get Shit Done is a full stack project planning application that allows a user to fully CRUD Projects, Tasks, and Materials. The app leverages React Hooks, Next JS Routes, Django, Google Firebase for Auth and Bootstrap to give the user a clean experience to keep several projects organized at once.',
         gitHubLink: 'https://github.com/ericlfrey/PROject-Planner#readme',
         loomLink: 'https://www.loom.com/share/33bdbc8002d44138a09159332885b55f?sid=3d6ebd80-ad1b-4e19-876e-bf1c51f2ffde',
-        deployedLink: 'http://gsd.ericlfrey.com',
+        deployedLink: '',
         logoImg: '/images/gsd-logo.png'
     },
     {
@@ -15,7 +15,7 @@ export const projectData = [
         description: 'The Soap Dish is a single page CRUD app built with a React/NextJS front end, and Python/Django back end that allows soapers to create, share, and save favorite soap recipes. TSD uses Bootstrap for both desktop and mobile responsive design.',
         gitHubLink: "https://github.com/ericlfrey/soap-dish-client#readme",
         loomLink: 'https://www.loom.com/share/25365a2897584b56932bf286f0173da8?sid=7f3c22d6-ed7a-485e-8c05-5ce01badef3f',
-        deployedLink: 'http://soap-dish.ericlfrey.com/',
+        deployedLink: '',
         logoImg: '/images/sd-logo.png'
     },
     {
